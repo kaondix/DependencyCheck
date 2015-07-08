d7e11c27ed3d2fb215d59179c6188147b65602ac
Jeremy Long <jeremy.long@gmail.com>
2015-07-08 06:20:19 -0400

    reorganized code, made the database file name version independent so that upgrades can be made, and implemented a CPE updates per issue #149

---
+++ dependency-check-core/src/test/java/org/owasp/dependencycheck/data/update/nvd/NvdCve_2_0_HandlerTest.java
@@ -0,0 +1,79 @@
+/*
+ * This file is part of dependency-check-core.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) 2012 Jeremy Long. All Rights Reserved.
+ */
+package org.owasp.dependencycheck.data.update.nvd;
+
+import org.owasp.dependencycheck.data.update.nvd.NvdCve20Handler;
+import java.io.File;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.owasp.dependencycheck.BaseTest;
+
+/**
+ *
+ * @author Jeremy Long
+ */
+public class NvdCve_2_0_HandlerTest {
+
+    public NvdCve_2_0_HandlerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testParse() {
+        Throwable results = null;
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+
+            //File file = new File(this.getClass().getClassLoader().getResource("nvdcve-2.0-2012.xml").getPath());
+            File file = BaseTest.getResourceAsFile(this, "nvdcve-2.0-2012.xml");
+
+            NvdCve20Handler instance = new NvdCve20Handler();
+
+            saxParser.parse(file, instance);
+        } catch (Throwable ex) {
+            results = ex;
+        }
+        assertTrue("Exception thrown during parse of 2012 CVE version 2.0?", results == null);
+        if (results != null) {
+            System.err.println(results);
+        }
+
+    }
+}
