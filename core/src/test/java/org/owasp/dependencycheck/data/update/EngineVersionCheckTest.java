eb2a265eb89cda7453f37bcdcb85fd36bddfee03
Michael Nielson <safetytrick@gmail.com>
2018-03-26 16:40:14 -0600

    remove deprecated mockit usage to fix build

--- core/src/test/java/org/owasp/dependencycheck/data/update/EngineVersionCheckTest.java
+++ core/src/test/java/org/owasp/dependencycheck/data/update/EngineVersionCheckTest.java
@@ -16,6 +16,8 @@
 package org.owasp.dependencycheck.data.update;
 
 import java.util.Properties;
+
+import mockit.Deencapsulation;
 import mockit.Mock;
 import mockit.MockUp;
 import org.joda.time.DateTime;
@@ -64,7 +66,7 @@ public class EngineVersionCheckTest extends BaseTest {
      */
     @Test
     public void testShouldUpdate() throws Exception {
-        DatabaseProperties properties = new MockUp<DatabaseProperties>() {
+        new MockUp<DatabaseProperties>() {
             final private Properties properties = new Properties();
             
             @Mock
@@ -77,9 +79,11 @@ public class EngineVersionCheckTest extends BaseTest {
                 return properties.getProperty(key);
             }
             
-        }.getMockInstance();
-        
-        String updateToVersion = "1.2.6";
+        };
+
+        DatabaseProperties properties = Deencapsulation.newUninitializedInstance(DatabaseProperties.class);
+
+                String updateToVersion = "1.2.6";
         String currentVersion = "1.2.6";
         
         long lastChecked = dateToMilliseconds("2014-12-01");
